0f6099b576c6373c4b30b45de37f3bb9b80d50dc
Zachary Pierson <zacpiersonhehe@gmail.com>
2018-03-02 15:47:20 -0600

    Testing the button portion of the library.

--- src/ui/designer_ui.cpp
+++ src/ui/designer_ui.cpp
@@ -1,5 +1,16 @@
 #include "designer_ui.hpp"
+#include <callista/ui/document_window.hpp>
+#include <callista/ui/button.hpp>
+
+
+Button *b;
+void destroy_button() {
+	button_destroy(b);
+}
 
 void setup_document_window(DocumentWindow *window) {
-	
+	b = button_create(BUTTONKIND_LABEL);
+	button_set_label(b, "Test!");
+	button_set_parent(b, window);
+	button_register_callback(b, destroy_button);
 }
